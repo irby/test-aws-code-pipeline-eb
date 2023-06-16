7e7d458083b0d5f62a04134d4273eb2c708fa296
Matthew H. Irby <matt.irby@outlook.com>
2023-06-16 14:16:32 -0400

    Remove bin and obj. Update weather controller to return env variable

--- AwsPipelineDemo/Controllers/WeatherForecastController.cs
+++ AwsPipelineDemo/Controllers/WeatherForecastController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace AwsPipelineDemo.Controllers
@@ -26,12 +27,13 @@ namespace AwsPipelineDemo.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            var environment = Environment.GetEnvironmentVariable("TEST_VAR") ?? "undefined";
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = environment
             })
             .ToArray();
         }
